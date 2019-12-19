0d34c55457c6eea65de2079000bdfee25da46c5a
Shaun Savage <shaunx.savage@intel.com>
2019-12-18 19:45:26 -0800

    btr_helper

--- mktests.sh
+++ mktests.sh
@@ -16,7 +16,13 @@ echo "DummyTest"
 echo "$(ls -l)"
 export TEST_RESULT_LOG="\${BTR_CUR_TEST_LOG_DIR}/test-metrics.txt"
 printf "\${TEST_RESULT_LOG}"
-touch \${TEST_RESULT_LOG}
+cat << EOF >> \${TEST_RESULT_LOG}
+ACTUAL_PERFORMANCE_FPS=30
+BLOB_ESTIMATE_FPS=40
+CRC_MATCH="PASS"
+NN_PERFORMANCE_CLK=100
+BLOB_ESTIMATE_us=500
+EOF
 exit(0)
 __END_TEST_APP
 chmod +x "${test_app}"
