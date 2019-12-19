8b361acaede7f464ae32cd3a4865508cbdbb0736
Shaun Savage <shaunx.savage@intel.com>
2019-12-18 17:39:06 -0800

    added prints

--- mktests.sh
+++ mktests.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 test_app="t_dummy_test"
+printf "make t_dummy_test\n"
+
+cd ${WORK_ROOT}/test
 
 cat << __END_TEST_APP > "${test_app}" 
 #!/bin/bash
 set -eu
 export WORK_ROOT="${HOME}/work"
-TEST_DIR=${WORK_ROOT}/${TEST_BASE_DIR}
+TEST_DIR=\${WORK_ROOT}/\${TEST_BASE_DIR}
+cd ${TEST_DIR}
 echo "DummyTest\n"
 echo $(ls -l)
 
-
 __END_TEST_APP
+
+printf "wrote file"
 
